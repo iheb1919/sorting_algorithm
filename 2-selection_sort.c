80c1078e0fc68344d186beb401e0aba89090be32
iheb1919 <ihebmejri14@gmail.com>
2020-02-04 20:48:56 +0100

    ff

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -9,12 +9,12 @@
 void selection_sort(int *array, size_t size)
 {
 size_t i, j, min;
-int aux ;
-for (i = 0; i < size-1; i++)
+int aux;
+for (i = 0; i < size - 1; i++)
 {
 min = i;
 for (j = i + 1; j < size; j++)
-{  
+{
 if (array[j] < array[min])
 {
 min = j;
