4f777b9fdb83472962fbe41e6c192919a45b1585
iheb1919 <ihebmejri14@gmail.com>
2020-02-04 17:46:07 +0100

    ff

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,43 @@
+#include "sort.h"
+/**
+ * insertion_sort_list - function that sorts  integers in ascending order
+ * @list: pointer to pointer of list
+ *
+ *
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+listint_t *nodenext, *node, *node3;
+if (*list && (*list)->next)
+{
+nodenext = (*list)->next;
+while (nodenext)
+{
+node = nodenext->prev;
+if (node)
+{
+if (nodenext->n < node->n)
+{
+node3 = node->prev;
+if (node3)
+node3->next = nodenext;
+else
+*list = nodenext;
+nodenext->prev = node3;
+node->prev = nodenext;
+node->next = nodenext->next;
+if (node->next)
+node->next->prev = node;
+nodenext->next = node;
+node = node->prev;
+ if (nodenext->prev)
+nodenext = nodenext->prev;
+print_list(*list);
+}
+}
+if (nodenext)
+nodenext = nodenext->next;
+}
+}
+}
--- sort.h
+++ sort.h
@@ -1,6 +1,8 @@
 #ifndef SORT_H
 #define SORT_H
 
+#include <stddef.h>
+#include <stdio.h>
 
 
 
@@ -22,4 +24,5 @@ void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
+void swap_node(listint_t **head, listint_t *node1, listint_t *node2);
 #endif
