8e61c84a6c9124fada862d9baed1ce66b69b5685
student1043 <1043@holbertonschool.com>
2020-02-05 19:56:48 +0100

    Update 100-shell_sort.c

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -13,7 +13,7 @@ int p = 1, i, temp;
 
 while (p < (int)(size))
 p = (3 * p) + 1;
-for (p = (int)size/2; p > 0; p /= 2)
+for (p = (p - 1) / 3; p > 0; (p - 1) / 3)
 {
 for (i = p; i < (int)size; i += 1)
 {
