5bd12f2139a0e685dc49ab88753e8e368a7fc166
iheb1919 <ihebmejri14@gmail.com>
2020-02-05 01:54:54 +0100

    ff

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -2,14 +2,11 @@
 /**
  * insertion_sort_list - function that sorts  integers in ascending order
  * @list: pointer to pointer of list
- *
- *
  */
-
 void insertion_sort_list(listint_t **list)
 {
 listint_t *nodenext, *node, *node3;
-if (*list && (*list)->next)
+if (*list != NULL && (*list)->next != NULL)
 {
 nodenext = (*list)->next;
 while (nodenext)
